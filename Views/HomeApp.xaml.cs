effe01fdd1a7e94e9be2989cc0ee43fbbdc0daa7
gyoora <geovanavalentim44@gmail.com>
2025-11-23 01:08:49 -0300

    Usando localização do dispositivo.

--- Views/HomeApp.xaml.cs
+++ Views/HomeApp.xaml.cs
@@ -1,5 +1,5 @@
-using MauiAppProva2.Services;
 using MauiAppProva2.Models;   
+using MauiAppProva2.Services;
 
 namespace MauiAppProva2.Views;
 
@@ -28,25 +28,21 @@ public partial class HomeApp : ContentPage
                     
                     lblCoordenadas.Text = $"Lat: {t.lat} | Lon: {t.lon}";
 
-                    // 3. Nascer e Pôr do Sol (Dados do prof)
                     lblSol.Text = $"{t.sunrise} | {t.sunset}";
 
-                    // 4. Temperaturas Máx e Mín (Dados do prof)
-                    // Se algum vier nulo, mostra "?", senão mostra o número
+
                     string max = t.temp_max.HasValue ? t.temp_max.ToString() : "?";
                     string min = t.temp_min.HasValue ? t.temp_min.ToString() : "?";
 
                     lblTemperatura.Text = $"{max}° / {min}°";
 
-                    // 5. Descrição (Extra para ficar bonito)
                     lblDescricao.Text = t.description;
 
-                    // 6. Vento (Extra para preencher o espaço)
                     lblVento.Text = $"{t.speed} km/h";
                 }
                 else
                 {
-                    // Caso não encontre dados
+
                     lblDescricao.Text = "Sem dados de Previsão";
                     await DisplayAlert("Ops", "Cidade não encontrada", "OK");
                 }
@@ -59,6 +55,47 @@ public partial class HomeApp : ContentPage
         catch (Exception ex)
         {
             await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+    private async void LocalizacaoClicked(object sender, EventArgs e)
+    {
+        try
+        {
+            GeolocationRequest request = new GeolocationRequest(
+                GeolocationAccuracy.Medium, 
+                TimeSpan.FromSeconds(10)
+            );
+
+            Location? local = await Geolocation.Default.GetLocationAsync(request);
+
+            if (local != null)
+            {
+                string local_disp = $"Latitude: {local.Latitude}, Longitude: {local.Longitude}";
+
+                lblCoordenadasLocationGPS.Text = local_disp;
+            }
+            else
+            {
+                await DisplayAlert("Erro", "Não foi possível obter a localização.", "OK");
+            }
+
+        }
+        catch (FeatureNotSupportedException fnsEx)
+        {
+            await DisplayAlert("Erro: Dispositivo não suporta", fnsEx.Message, "OK");
+        }
+        catch (FeatureNotEnabledException fneEx)
+        {
+            await DisplayAlert("Erro: Localização desabilitada", fneEx.Message, "OK");
+        }
+        catch (PermissionException pEx)
+        {
+            await DisplayAlert("Erro: Permissão da localização", pEx.Message, "OK");
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Erro", ex.Message, "OK");
         }
     }
 }
